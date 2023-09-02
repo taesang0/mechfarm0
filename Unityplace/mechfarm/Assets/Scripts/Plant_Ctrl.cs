0cd58fae80bde05267d50b67557d093bce6e918e
Lee Taesang <dandy0113@gmail.com>
2023-09-02 18:17:57 +0900

    식물 상태 측정하는거 이상해서 고침/ 식물 상태별 애니메이션(dry, hot, cold)

--- Unityplace/mechfarm/Assets/Scripts/Plant_Ctrl.cs
+++ Unityplace/mechfarm/Assets/Scripts/Plant_Ctrl.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Plant_Ctrl : MonoBehaviour
 {
     private FB_Read readFBScript; 
+    private Read_Plant_Database PlantDBScript;
     string max_state;
     float max_value;
     private Animator animator;
@@ -18,6 +19,7 @@ public class Plant_Ctrl : MonoBehaviour
     void Start()
     {
         readFBScript = GameObject.Find("ReadData").GetComponent<FB_Read>();
+        PlantDBScript = GameObject.Find("Plant_Database").GetComponent<Read_Plant_Database>();
 
         if (readFBScript == null)
         {
@@ -39,44 +41,44 @@ public class Plant_Ctrl : MonoBehaviour
             yield return new WaitForSeconds(4.3f);
             if (readFBScript != null)
             {
-                max_value = 10.0f; // 그냥 +-10으로 기준잡음
+                max_value = 0.05f; // 그냥 +-10으로 기준잡음
 
-                if (Mathf.Abs(readFBScript.temperature-50.0f)>max_value)
+                if (Mathf.Abs((readFBScript.temperature-PlantDBScript.plantData.Temperature_best)/(PlantDBScript.plantData.Temperature_max-PlantDBScript.plantData.Temperature_min))>max_value)
                 {
-                    max_value = Mathf.Abs(readFBScript.temperature-50.0f);
-                    if (readFBScript.temperature-50.0f>0) max_state="temperature_H";
+                    max_value = Mathf.Abs((readFBScript.temperature-PlantDBScript.plantData.Temperature_best)/(PlantDBScript.plantData.Temperature_max-PlantDBScript.plantData.Temperature_min));
+                    if ((readFBScript.temperature-PlantDBScript.plantData.Temperature_best)>0) max_state="temperature_H";
                     else max_state="temperature_L";
                 }
 
-                if (Mathf.Abs(readFBScript.humidity-50.0f)>max_value)
+                if (Mathf.Abs((readFBScript.humidity-PlantDBScript.plantData.Humidity_best)/(PlantDBScript.plantData.Humidity_max-PlantDBScript.plantData.Humidity_min))>max_value)
                 {
-                    max_value = Mathf.Abs(readFBScript.humidity-50.0f);
-                    if (readFBScript.humidity-50.0f>0) max_state="humidity_H";
+                    max_value = Mathf.Abs((readFBScript.humidity-PlantDBScript.plantData.Humidity_best)/(PlantDBScript.plantData.Humidity_max-PlantDBScript.plantData.Humidity_min));
+                    if ((readFBScript.humidity-PlantDBScript.plantData.Humidity_best)>0) max_state="humidity_H";
                     else max_state="humidity_L";
                 }
-                else if (Mathf.Abs(readFBScript.soil_humidity-50.0f)>max_value)
+                if (Mathf.Abs((readFBScript.soil_humidity-PlantDBScript.plantData.Soil_humidity_best)/(PlantDBScript.plantData.Soil_humidity_max-PlantDBScript.plantData.Soil_humidity_min))>max_value)
                 {
-                    max_value = Mathf.Abs(readFBScript.soil_humidity-50.0f);
-                    if (readFBScript.soil_humidity-50.0f>0) max_state ="soil_humidity_H";
+                    max_value = Mathf.Abs((readFBScript.soil_humidity-PlantDBScript.plantData.Soil_humidity_best)/(PlantDBScript.plantData.Soil_humidity_max-PlantDBScript.plantData.Soil_humidity_min));
+                    if ((readFBScript.soil_humidity-PlantDBScript.plantData.Soil_humidity_best)>0) max_state ="soil_humidity_H";
                     else max_state= "soil_humidity_L";
                 }
-                else if (Mathf.Abs(readFBScript.lightness-50.0f)>max_value)
+                if (Mathf.Abs((readFBScript.lightness-PlantDBScript.plantData.Light_best)/(PlantDBScript.plantData.Light_max-PlantDBScript.plantData.Light_min))>max_value)
                 {
-                    max_value = Mathf.Abs(readFBScript.lightness-50.0f);
-                    if (readFBScript.lightness-50.0f>0) max_state ="lightness_H";
+                    max_value =Mathf.Abs((readFBScript.lightness-PlantDBScript.plantData.Light_best)/(PlantDBScript.plantData.Light_max-PlantDBScript.plantData.Light_min));
+                    if ((readFBScript.lightness-PlantDBScript.plantData.Light_best)>0) max_state ="lightness_H";
                     else max_state= "lightness_L";
 
                 }
                 animator_manager(animator,max_state);
 
             }
-            Debug.Log(max_state);
+            Debug.Log(max_value + " "+max_state);
         }
     }
 
     void animator_manager(Animator ani,string state)
     {
-        if (state == "soil_hummidity_H")
+        if (state == "soil_humidity_H")
         {
             ani.SetBool("dry",true);
             ani.SetBool("hot",false);
